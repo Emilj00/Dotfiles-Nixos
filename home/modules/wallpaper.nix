9aca24cef764f10a91cc65502b5bb0baca0b6d11
Emilj00 <106914054+Emilj00@users.noreply.github.com>
2025-06-15 18:07:17 +0200

    Wallpaper

--- home/modules/wallpaper.nix
+++ home/modules/wallpaper.nix
@@ -2,22 +2,6 @@
 
 {
   home.packages = lib.mkAfter [ 
-    pkgs.swww
+    pkgs.swaybg
   ];
-
-  systemd.user.services.swww = {
-    Unit = {
-      Description = "swww daemon";
-      After = [ "graphical-session.target" ];
-    };
-
-    Service = {
-      ExecStart = "${pkgs.swww}/bin/swww-daemon";
-      Restart = "on-failure";
-    };
-
-    Install = {
-      WantedBy = [ "default.target" ];
-    };
-  };
 }
