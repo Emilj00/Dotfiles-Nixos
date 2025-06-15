34566721153cd7c57403c8a8338158d67766acba
Emilj00 <106914054+Emilj00@users.noreply.github.com>
2025-06-15 18:06:54 +0200

    Autologin

--- hosts/modules/login-manager.nix
+++ hosts/modules/login-manager.nix
@@ -1,4 +1,8 @@
 {
   services.displayManager.sddm.enable = true;
+
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "emilj00";
+
   services.xserver.enable = true;
 }
