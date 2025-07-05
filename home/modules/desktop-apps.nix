97af6584fbe30480f21f3fe66b3eb40ddb176a03
Emilj00 <106914054+Emilj00@users.noreply.github.com>
2025-07-05 13:30:24 +0200

    Fixed mimemaps

--- home/modules/desktop-apps.nix
+++ home/modules/desktop-apps.nix
@@ -1,15 +1,19 @@
 { lib, pkgs, ... }:
 
-{
-  xdg.defaultApplications = {
-    x-scheme-handler.http = "app.zen_browser.zen.desktop";
-    x-scheme-handler.https = "app.zen_browser.zen.desktop";
-
-    # Default image viewer
-    "image/png"  = "org.gnome.gThumb.desktop";
-    "image/jpeg" = "org.gnome.gThumb.desktop";
-    "image/jpg"  = "org.gnome.gThumb.desktop";
-    "image/gif"  = "org.gnome.gThumb.desktop";
-    "image/bmp"  = "org.gnome.gThumb.desktop";
+let
+  apps = {
+    "x-scheme-handler/http"  = [ "app.zen_browser.zen.desktop" ];
+    "x-scheme-handler/https" = [ "app.zen_browser.zen.desktop" ];
+    "image/png"  = [ "org.gnome.gThumb.desktop" ];
+    "image/jpeg" = [ "org.gnome.gThumb.desktop" ];
+    "image/jpg"  = [ "org.gnome.gThumb.desktop" ];
+    "image/gif"  = [ "org.gnome.gThumb.desktop" ];
+    "image/bmp"  = [ "org.gnome.gThumb.desktop" ];
+  };
+in {
+  xdg.mimeApps = {
+    enable = true;
+    associations.added = apps;
+    defaultApplications = apps;
   };
 }
